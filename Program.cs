07eea5c96b6925c790b021ef5e8a2b852457643e
Jessica <jpanndora@gmail.com>
2025-10-03 19:53:27 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Digite a temperatura em Fahrenheit: ");
+double fahrenheit = Convert.ToDouble(Console.ReadLine());
+double celsius = (fahrenheit - 32) / 1.8;
+Console.WriteLine($"A temperatura em Celsius é: {celsius:F2}°C");
+
